282af8cc59c9864b1218740e3598c6f73ab805e4
Tomasz Mamala <tomaszmamala@gmail.com>
2023-10-17 10:33:10 +0200

    Fixed logout function

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Post, Res } from '@nestjs/common';
 import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('/auth')
 export class AuthController {
+  constructor(private readonly configService: ConfigService) {}
+
   @Post('logout')
   async logout(@Res() res: Response) {
-    res.clearCookie('jwt');
-    res.clearCookie('user_info');
+    res.clearCookie('jwt', {
+      domain: this.configService.get<string>('DOMAIN'),
+    });
+    res.clearCookie('user_info', {
+      domain: this.configService.get<string>('DOMAIN'),
+    });
     res.send({ statusCode: true, message: 'Logout success' });
   }
 }
